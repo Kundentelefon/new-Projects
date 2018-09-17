b8f4f53fc25c7d5fc22b29314be1835b258ec441
Kundentelefon <Kundentelefon@web.de>
2018-09-17 23:26:09 +0200

    first commit

---
+++ Start file/Assets/Scripts/FieldOfView.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//target & obstacle layer
+//script on controlled char
+public class FieldOfView : MonoBehaviour {
+
+    public float viewRadius;
+    public float viewAngle;
+
+    //take an angle and spit out the direction of that angle
+    public Vector3 DirFromAngle(float angleInDegrees)
+    {
+        //trigonometrie circle, unity starts with 0 on top and goes counter clockwise
+        //convert unity circle to trig circle, 90-x = swap sin with cos
+        return new Vector3(Mathf.Sin(angleInDegrees * Mathf.Deg2Rad), 0, Mathf.Cos(angleInDegrees * Mathf.Deg2Rad));
+    }
+
+
+
+}
